6beaaf093e15899b4e9643173ee4a577e9e59ebd
Mbn02 <Mohankumarbn02@gmail.com>
2022-03-15 20:04:59 +0530

    Added docker file

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -3,8 +3,8 @@
 def call() {
     echo "building the docker image..."
     withCredentials([usernamePassword(credentialsId: 'nexus-user', passwordVariable: 'PASSWORD', usernameVariable: 'USERNAME')]) {
-        sh 'docker build -t [nexus-ip]:[docker-port]/java-maven-app:1.0 .'
-        sh "echo $PASSWORD | docker login -u $USERNAME --password-stdin [nexus-ip]:[docker-port]"
-        sh 'docker push [nexus-ip]:[docker-port]/java-maven-app:1.0'
+        sh 'docker build -t 40.83.250.80:8083/java-maven-app:1.0 .'
+        sh "echo $PASSWORD | docker login -u $USERNAME --password-stdin 40.83.250.80:8083"
+        sh 'docker push 40.83.250.80:8083/java-maven-app:1.0'
     }
 }
